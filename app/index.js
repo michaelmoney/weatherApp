84cee90aef6f631fc2e0301cfa49b2b669a6a32f
Michal Pieniazek <michal.pieniazek@yougov.com>
2016-03-06 19:59:00 +0100

    Fetch data from OpenWather and display inside HTML template

--- app/index.js
+++ app/index.js
@@ -2,13 +2,51 @@
 
 var weatherApp = angular.module('weatherApp', []);
 
-weatherApp.directive('weatherApp', function () {
+weatherApp.directive('weatherApp', ['$http', '$httpParamSerializer', function ($http, $httpParamSerializer) {
     return {
         restrict: 'E',
         templateUrl: './weather-template.html',
         scope: true,
         link: function(scope, elem, attr) {
             scope.title = 'iWeather 0.0.1';
+            var getWeather = function (city, cc, lang, units) {
+                if (!city || !cc) {
+                    return console.error('City & CC missing...');
+                }
+                var url, params = $httpParamSerializer({
+                    q: city + ',' + cc,
+                    APPID: '6618cb01c3501a12296501fb525edd96',
+                    lang: lang,
+                    units: units
+                });
+                url = 'http://api.openweathermap.org/data/2.5/weather';
+                url += '?';
+                url += params;
+                $http({
+                    method: 'GET',
+                    url: url
+                }).then(function successCallback(response) {
+                    console.log('Weather received!');
+                    var data = response.data;
+                    scope.weather = {
+                        humidity: data.main.humidity,
+                        pressure: data.main.pressure,
+                        temp: data.main.temp,
+                        tempMax: data.main.temp_max,
+                        tempMin: data.main.temp_min,
+                        city: data.name,
+                        icon: data.weather[0].icon,
+                        description: data.weather[0].description,
+                        cc: data.sys.country
+                    };
+                }, function errorCallback(response) {
+                    console.log('Oh crap! Error:', response);
+                });
+            };
+
+            getWeather('Warsaw', 'pl', 'pl', 'metric');
+
+
         }
     };
-});
+}]);
