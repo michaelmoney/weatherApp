101699bd8717a9b1c892bc8ceb85ad10ae0d63bb
Michal Pieniazek <michal.pieniazek@yougov.com>
2016-03-06 19:17:45 +0100

    Create main directive for application - "weather-app" directive + HTML template

--- app/index.js
+++ app/index.js
@@ -2,3 +2,13 @@
 
 var weatherApp = angular.module('weatherApp', []);
 
+weatherApp.directive('weatherApp', function () {
+    return {
+        restrict: 'E',
+        templateUrl: './weather-template.html',
+        scope: true,
+        link: function(scope, elem, attr) {
+            scope.title = 'iWeather 0.0.1';
+        }
+    };
+});
